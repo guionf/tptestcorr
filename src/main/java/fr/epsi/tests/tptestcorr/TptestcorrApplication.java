0a97a3b50de6b6b121678349e687850c0b4b0376
Guillaume ONFRAY <guillaume.onfray@epsi.fr>
2021-03-05 12:13:10 +0100

    Initial commit

---
+++ src/main/java/fr/epsi/tests/tptestcorr/TptestcorrApplication.java
@@ -0,0 +1,13 @@
+package fr.epsi.tests.tptestcorr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TptestcorrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TptestcorrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/epsi/tests/tptestcorr/TptestcorrApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.epsi.tests.tptestcorr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TptestcorrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
